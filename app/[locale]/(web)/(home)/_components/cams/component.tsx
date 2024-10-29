6e3659a0397879758a8b4c823d1bd7cc5f1d9cbb
Mahmoud Zalt <mahmoud@company.ccc>
2024-10-29 08:54:58 +0100

    add: Component of Model

--- app/[locale]/(web)/(home)/_components/cams/component.tsx
+++ app/[locale]/(web)/(home)/_components/cams/component.tsx
@@ -3,27 +3,20 @@ import { sleep } from "@/lib/utils/sleep";
 
 import { Link } from 'next-view-transitions';
 import Image from "next/image";
+import { Model } from "./model";
 
 export default async function HomeCamsComponent() {
   return (
     <div className="flex flex-col gap-10">
       <div className="flex flex-col gap-6">
         <p className="font-semibold text-4xl tracking-tighter">Empfohlene neue Cams</p>
-        <div className="flex flex-col gap-3">
-          <div className="w-60 h-32 bg-blue-200/75 rounded-sm relative">
-            { /*
-              <Image
-              className="rounded-sm"
-              src=""
-              alt="Cam Model Preview"
-              fill
-            /> */}
-          </div>
-
-          <div className="flex flex-col tracking-tight">
-            <Link className="text-xl font-semibold hover:underline underline-offset-2" href="/de/erdal-goektas">Erdal Göktas</Link>
-            <h1 className="text-sm font-medium text-white/45 tracking-[-0.0125rem]">2500 Zuschauer</h1>
-          </div>
+        <div className="flex flex-wrap gap-2 w-full">
+          {/* <div className="w-64 h-36 bg-blue-200/75 rounded-md relative flex items-end py-2 px-3">
+            <div className="flex flex-col tracking-tight">
+              <Link className="text-sm font-semibold underline-offset-2" href="/de/erdal-goektas">ErdalGöktas</Link>
+            </div>
+          </div> */}
+          <Model name="divancoban" />
         </div>
       </div>
     </div>
